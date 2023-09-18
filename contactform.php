c707b3cb909a303e7bf770d2a3cea2b6716a1204
Your Name <you@example.com>
2023-09-18 22:38:55 +0700

    contactform.php

--- contactform.php
+++ contactform.php
@@ -11,7 +11,7 @@ if (isset($_POST['submit'])) {
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
  mail($mailTo, $txt, $headers);
- header("location: index.php?mailsend");  
+ header("location: index.html?mailsend");  
     }
 
  
