bd7cdc53d859bd937a3d42cea3917eee27b50f2a
Your Name <you@example.com>
2023-09-18 20:21:37 +0700

    edit contactform.php

--- contactform.php
+++ contactform.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $mailTo = "https://mail.google.com/chat/u/0/#browse/chat/q=";
+    $mailTo = "amitta.ratt@outlook.com";
     $headers = "From: ".$email;
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
