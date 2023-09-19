be3e7b38db2661e6ac29d15113cdf4463351672b
Your Name <you@example.com>
2023-09-19 20:03:03 +0700

    contactform.php

--- contactform.php
+++ contactform.php
@@ -1,18 +1,37 @@
 <?php
-
 if (isset($_POST['submit'])) {
-
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    $mailTo = "Amitta.Ratt@outlook.co.th";
-    $headers = "From: ".$email;
-    $txt = "You have received an email from ".$name.".\n\n".$message;
-
- mail($mailTo, $txt, $headers);
- header("location: index.html?mailsend");  
+    // Check for empty fields
+    if (empty($name) || empty($email) || empty($message)) {
+        header("location: index.html?error=emptyfields");
+        exit();
     }
 
- 
-    
+    // Validate the email address
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("location: index.html?error=invalidemail");
+        exit();
+    }
+
+    $mailTo = "Amitta.Ratt@outlook.co.th";
+    $subject = "Contact Form Submission from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $txt = "You have received an email from $name:\n\n$message";
+
+    if (mail($mailTo, $subject, $txt, $headers)) {
+        header("location: index.html?mailsend=success");
+    } else {
+        header("location: index.html?mailsend=error");
+    }
+} else {
+    // Redirect or handle non-submission as needed
+    header("location: index.html");
+}
+?>
